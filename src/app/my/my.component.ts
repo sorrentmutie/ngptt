a158008258ff9300ae7b405d2bb5307bd620a717
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2024-02-01 13:17:28 +0100

    final fixes

---
+++ src/app/my/my.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      my works!
+    </p>
+  `,
+  styles: ``
+})
+export class MyComponent {
+
+}
--- src/app/shared/components/welcome/welcome.component.ts
+++ src/app/shared/components/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ViewContainerRef, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { MyComponent } from '../../../my/my.component';
 
 @Component({
   selector: 'app-welcome',
@@ -10,17 +11,30 @@ import { AuthService } from '../../services/auth.service';
     </p>
     <button class="btn btn-info" (click)="navigate()">Naviga</button>
     <button class="btn btn-info" (click)="login()">Login</button>
-
+    <button (click)="creaComponente()">Crea My</button>
+    <ng-container #cont>
+    </ng-container>
 
 
   `,
   styles: ``
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements AfterViewInit{
     isLogged = false;
+    @ViewChild('cont', {static: false, read: ViewContainerRef}) 
+          cont: ViewContainerRef | any;
     constructor(private router: Router, private authService: AuthService){
 
     }
+  ngAfterViewInit(): void {
+   
+  }
+
+creaComponente(){
+  this.cont.createComponent(MyComponent);
+}
+
+
     navigate(){
         this.router.navigate(['random']);
     }
